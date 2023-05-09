da437a35870c16fbc2c5e4b736674b87cdc2c8a4
Nikolay <nickolay.myskin@yandex.ru>
2023-05-09 14:24:32 +0300

    Cellular Automaton logic update, added class for dynamic data storing

---
+++ Models/SimulationData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace MFFSuCA.Models; 
+
+// TODO: Charts window, gather data from simulation
+
+public class SimulationData {
+    public int TotalVegetation { get; set; }
+    
+    public List<int> BurningArea { get; set; } = new();
+    
+    public List<int> BurnedArea { get; set; } = new();
+    
+    public List<int> BurningSpeed { get; set; } = new();
+    
+    public List<double> DamagedVegetation { get; set; } = new();
+}
--- Windows/SimulationWindow.xaml.cs
+++ Windows/SimulationWindow.xaml.cs
@@ -74,45 +74,99 @@ public partial class SimulationWindow {
     private async Task SimulateAsync() {
         var random = new Random();
         _simulationCancellationTokenSource = new CancellationTokenSource();
-        var simulationSpeedMultiplier = (SpeedComboBox.SelectedItem as string) switch {
-            "x1" => 1,
-            "x2" => 2,
-            "x4" => 4,
-            "x8" => 8,
-            _ => 1
-        };
-        var simulationSpeed = 1000 / simulationSpeedMultiplier;
+        
         while (true) {
             if (_simulationCancellationTokenSource.IsCancellationRequested) {
                 break;
             }
+            
+            var simulationSpeedMultiplier = ((ComboBoxItem)SpeedComboBox.SelectedItem).Content.ToString() switch {
+                "x1" => 1,
+                "x2" => 2,
+                "x4" => 4,
+                "x8" => 8,
+                _ => 1
+            };
+            var simulationSpeed = 1000 / simulationSpeedMultiplier;
 
-            foreach (var (rect, cell) in _cells) {
-                if (cell.TerrainType is BrushType.Clear or BrushType.Water or BrushType.Mountain) {
-                    continue;
-                }
-
-                switch (cell.BurningState) {
-                    case BurningState.Burned:
+            var updatedCells = new List<Tuple<int, int, Rectangle, Cell>>();
+            for (var i = 0; i < _rectArray.Count; i++) {
+                for (var j = 0; j < _rectArray[0].Count; j++) {
+                    var rect = _rectArray[i][j];
+                    var cell = _cells[rect];
+                    if (cell.TerrainType is BrushType.Clear or BrushType.Water or BrushType.Mountain) {
                         continue;
-                    case BurningState.Burning when ++cell.BurningTime > cell.MaximumBurningTime:
-                        cell.BurningState = BurningState.Burned;
-                        await Dispatcher.InvokeAsync(() => {
-                            rect.Fill = new SolidColorBrush(Colors.Black);
-                            rect.StrokeThickness = 0;
-                        });
-                        continue;
-                }
+                    }
 
-                var neighbours = GetNeighbours(cell);
-                // TODO: CHECK INNER EXCEPTIONS (MAYBE ref IS THE PROBLEM)
-                var probability = UpdateProbability(cell, ref neighbours);
-                if (!(random.NextDouble() < probability))
-                    continue;
-                cell.BurningState = BurningState.Burning;
+                    switch (cell.BurningState) {
+                        case BurningState.Burned:
+                            continue;
+                        case BurningState.Burning when ++cell.BurningTime > cell.MaximumBurningTime:
+                            // cell.BurningState = BurningState.Burned;
+                            // await Dispatcher.InvokeAsync(() => {
+                            //     rect.Fill = new SolidColorBrush(Colors.Black);
+                            //     rect.StrokeThickness = 0;
+                            // });
+                            var newCell = new Cell {
+                                TerrainType = cell.TerrainType,
+                                X = cell.X,
+                                Y = cell.Y
+                            };
+                            newCell.BurningState = BurningState.Burned;
+                            var newRect = new Rectangle {
+                                Width = RectSize,
+                                Height = RectSize,
+                                Fill = new SolidColorBrush(Colors.Black),
+                                Stroke = new SolidColorBrush(Colors.Black),
+                                StrokeThickness = 0
+                            };
+                            Canvas.SetLeft(newRect, Canvas.GetLeft(rect));
+                            Canvas.SetTop(newRect, Canvas.GetTop(rect));
+                            updatedCells.Add(new Tuple<int, int, Rectangle, Cell>(i, j, newRect, newCell));
+                            continue;
+                        case BurningState.Burning:
+                            continue;
+                    }
+
+                    var neighbours = GetNeighbours(cell, rect);
+                    // TODO: Add probability of extinguishing
+                    var probability = UpdateProbability(cell, ref neighbours);
+                    if (!(random.NextDouble() < probability))
+                        continue;
+                    // cell.BurningState = BurningState.Burning;
+                    // await Dispatcher.InvokeAsync(() => {
+                    //     rect.Stroke = new SolidColorBrush(Colors.Red);
+                    //     rect.StrokeThickness = 2;
+                    // });
+                    var newCell2 = new Cell {
+                        TerrainType = cell.TerrainType,
+                        X = cell.X,
+                        Y = cell.Y,
+                        BurningState = BurningState.Burning
+                    };
+                    var newRect2 = new Rectangle {
+                        Width = RectSize,
+                        Height = RectSize,
+                        Fill = rect.Fill,
+                        Stroke = new SolidColorBrush(Colors.Red),
+                        StrokeThickness = 2
+                    };
+                    Canvas.SetLeft(newRect2, Canvas.GetLeft(rect));
+                    Canvas.SetTop(newRect2, Canvas.GetTop(rect));
+                    updatedCells.Add(new Tuple<int, int, Rectangle, Cell>(i, j, newRect2, newCell2));
+                }
+            }
+            
+            if (updatedCells.Count == 0)
+                // TODO: Display info about simulation end, disable buttons, write data and charts to files
+                break;
+            
+            foreach (var (i, j, rect, cell) in updatedCells) {
                 await Dispatcher.InvokeAsync(() => {
-                    rect.Stroke = new SolidColorBrush(Colors.Red);
-                    rect.StrokeThickness = 2;
+                    SimulationCanvas.Children.Remove(_rectArray[i][j]);
+                    _rectArray[i][j] = rect;
+                    _cells[rect] = cell;
+                    SimulationCanvas.Children.Add(_rectArray[i][j]);
                 });
             }
 
@@ -122,45 +176,52 @@ public partial class SimulationWindow {
         }
     }
 
-    private List<Cell> GetNeighbours(Cell cell) {
-        var neighbours = new List<Cell>();
-        var x = cell.X;
-        var y = cell.Y;
-        var maxX = (_rectArray.Count - 1) * RectSize;
-        var maxY = (_rectArray[0].Count - 1) * RectSize;
-        if (x > 0) {
-            neighbours.Add(_cells[_rectArray[x - 1][y]]);
-        }
+    private List<Cell> GetNeighbours(Cell cell, Rectangle rect) {
+        try {
+            var neighbours = new List<Cell>();
+            // find indexes of rect in _rectArray
+            var x = _rectArray.FindIndex(row => row.Contains(rect));
+            var y = _rectArray[x].FindIndex(r => r == rect);
+            var maxX = _rectArray.Count - 1;
+            var maxY = _rectArray[0].Count - 1;
+            if (x > 0) {
+                neighbours.Add(_cells[_rectArray[x - 1][y]]);
+            }
 
-        if (x < maxX) {
-            neighbours.Add(_cells[_rectArray[x + 1][y]]);
-        }
+            if (x < maxX) {
+                neighbours.Add(_cells[_rectArray[x + 1][y]]);
+            }
 
-        if (y > 0) {
-            neighbours.Add(_cells[_rectArray[x][y - 1]]);
-        }
+            if (y > 0) {
+                neighbours.Add(_cells[_rectArray[x][y - 1]]);
+            }
 
-        if (y < maxY) {
-            neighbours.Add(_cells[_rectArray[x][y + 1]]);
-        }
+            if (y < maxY) {
+                neighbours.Add(_cells[_rectArray[x][y + 1]]);
+            }
 
-        if (x > 0 && y > 0) {
-            neighbours.Add(_cells[_rectArray[x - 1][y - 1]]);
-        }
+            if (x > 0 && y > 0) {
+                neighbours.Add(_cells[_rectArray[x - 1][y - 1]]);
+            }
 
-        if (x > 0 && y < maxY) {
-            neighbours.Add(_cells[_rectArray[x - 1][y + 1]]);
-        }
+            if (x > 0 && y < maxY) {
+                neighbours.Add(_cells[_rectArray[x - 1][y + 1]]);
+            }
 
-        if (x < maxX && y > 0) {
-            neighbours.Add(_cells[_rectArray[x + 1][y - 1]]);
-        }
+            if (x < maxX && y > 0) {
+                neighbours.Add(_cells[_rectArray[x + 1][y - 1]]);
+            }
 
-        if (x < maxX && y < maxY) {
-            neighbours.Add(_cells[_rectArray[x + 1][y + 1]]);
-        }
+            if (x < maxX && y < maxY) {
+                neighbours.Add(_cells[_rectArray[x + 1][y + 1]]);
+            }
 
-        return neighbours;
+            return neighbours;
+        }
+        catch (Exception e) {
+            Console.WriteLine(e);
+            throw;
+        }
     }
 
     private void UpdateTime() {
@@ -192,7 +253,7 @@ public partial class SimulationWindow {
     private double GetWindFactor(Cell cell, ref List<Cell> neighbours) {
         // take into account all burning neighbours from the given direction
         var windSpeed = WindSpeedSlider.Value;
-        var windDirection = WindDirectionComboBox.SelectedItem as string;
+        var windDirection =  ((ComboBoxItem) WindDirectionComboBox.SelectedItem).Content.ToString();
         var burningNeighbours = neighbours.Where(neighbour => neighbour.BurningState == BurningState.Burning).ToList();
         Dictionary<string, double> windFactors = new() {
             { "North", burningNeighbours.Count(neighbour => cell.Y < neighbour.Y && neighbour.X == cell.X) },
@@ -307,8 +368,6 @@ public partial class SimulationWindow {
 
     private void PauseButton_Click(object sender, RoutedEventArgs e) {
         _simulationCancellationTokenSource.Cancel();
-        _simulationTask?.Wait();
-        Dispatcher.ExitAllFrames();
         _isPaused = true;
         StartButton.IsEnabled = true;
         PauseButton.IsEnabled = false;
@@ -317,8 +376,6 @@ public partial class SimulationWindow {
 
     private void StopButton_Click(object sender, RoutedEventArgs e) {
         _simulationCancellationTokenSource.Cancel();
-        _simulationTask?.Wait();
-        Dispatcher.ExitAllFrames();
         _isPaused = false;
         StartButton.Content = "Start";
         StartButton.IsEnabled = true;
